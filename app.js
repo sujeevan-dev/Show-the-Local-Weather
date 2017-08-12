afcdbebd3c4e5cfd351799ba019a566f2199d4b0
Sujeevan <psujeevan@gmail.com>
2017-08-12 08:59:29 +0500

    icons and temperature converter need to be fixed

--- app.js
+++ app.js
@@ -29,87 +29,30 @@ function weather() {
     }
 
 //Button functions
-$('#fah').prop('disabled', true);
-$('#cel').prop('disabled', false);
+var result = '';
 
-var fah = document.getElementById("fah");
-var cel = document.getElementById("cel");
-var temp = document.getElementById("temp");
+$(document).ready(function() {
+	$.ajax({
+		success: function(data) {
+			$('#temp').html(JSON.stringify(data));
+			weather(data);
+			result = data;
+		}
+	});
+});
 
-function convert() {
+function fToC() {
+	var x = parseFloat(document.getElementById('temp').val);
+    var result = (x-32) * (5/9);
+    return result;
 
-if(cel.disabled == true) {
-    fah.value -32 * 5 / 9;
-    cel.value = Math.round(temp.value);
-} else if(fah.disabled == false) {
-    cel.value * 9 / 5 + 32;
-    fah.value = Math.round(temp.value);
-    }
+    document.getElementById('temp').innerHTML = result;
 
 }
-
-/*//Celcius and Ferheneit converter
-function convert(degree) {
-
-    if (degree == "F") {
-        x = (document.getElementById("f").value -32) * 5 / 9;
-        document.getElementById("c").value = Math.round(x);
-        
-    } else {
-        x = document.getElementById("c").value * 9 / 5 + 32;
-        document.getElementById("f").value = Math.round(x);
-    }
-}
-/icons
-	function showIcons () {
-	var icon = document.getElementById('#icon');
-	var data = document.getElementById('#icon').html(data.currently.icon);
-
-
-	switch (icon) {
-
-	case clear-day:
-        data = "<i class="wi wi-day-sunny"></i>";
-        break;
-    case clear-night:
-        data = "<i class="wi wi-night-clear"></i>";
-        break;
-    case snow:
-        symbol.html = "<i class="wi wi-snow"></i>";
-        break;
-    case sleet:
-        symbol.html = "<i class="wi wi-day-sleet"></i>";
-        break;
-    case wind:
-        symbol.html = "<i class="wi wi-windy"></i>";
-        break;
-    case hail:
-        symbol.html = "<i class="wi wi-hail"></i>";
-        break;
-    case fog:
-        symbol.html = "<i class="wi wi-fog"></i>";
-        break;
-    case cloudy:
-        symbol.html = "<i class="wi wi-cloud"></i>";
-        break;
-    case partly-cloudy-day:
-        symbol.html = "<i class="wi wi-day-cloudy-high"></i>";
-        break;
-    case partly-cloudy-night:
-        symbol.html = "<i class="wi wi-night-alt-cloudy"></i>";
-        break;
-    case thunderstorm:
-        symbol.html = "<i class="wi wi-thunderstorm"></i>";
-        break;
-    case tornado:
-        symbol.html = "<i class="wi wi-tornado"></i>";
-        break;
-	}
-}
-
-*/
-
-
-
 
 weather();
+
+
+/*
+*icons and temperature converter need to be fixed
+*/
